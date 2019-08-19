8b9ecc601671c7a56bf9ebf7dd00e57753dcb66d
dcDalin <mcdalinoluoch@gmail.com>
2019-08-19 14:24:36 +0300

    Connect to mongo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import express from 'express';
-
 import { ApolloServer } from 'apollo-server-express';
+import mongoose from 'mongoose';
 import typeDefs from './graphql/typeDefs';
 import resolvers from './graphql/resolvers';
+import config from './config';
 
-const app = express();
+const ENV_VAR = config.get(process.env.NODE_ENV);
 
-const PORT = 4000;
+const PORT = process.env.PORT || ENV_VAR.APP_PORT;
 
-const server = new ApolloServer({ typeDefs, resolvers });
+(async () => {
+  try {
+    const app = express();
 
-server.applyMiddleware({ app });
+    app.disable('x-powered-by');
 
-// eslint-disable-next-line no-console
-app.listen({ port: PORT }, () => console.log(`Server reqdy at http://localhost:${PORT}${server.graphqlPath}`));
+    const server = new ApolloServer({ typeDefs, resolvers });
+
+    server.applyMiddleware({ app });
+
+    await mongoose.connect(ENV_VAR.MONGO_URL, { useNewUrlParser: true });
+
+    // eslint-disable-next-line no-console
+    app.listen({ port: PORT }, () => console.log(`Server reqdy at http://localhost:${PORT}${server.graphqlPath}`));
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+})();
