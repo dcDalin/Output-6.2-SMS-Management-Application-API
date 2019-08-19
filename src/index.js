fdb81c8afc93c3845ccbbdf3cc1d534e56327c4e
dcDalin <mcdalinoluoch@gmail.com>
2019-08-19 13:08:15 +0300

    Setup graphql server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
+
 const app = express();
 
 const PORT = 4000;
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+const server = new ApolloServer({ typeDefs, resolvers });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.applyMiddleware({ app });
+
+// eslint-disable-next-line no-console
+app.listen({ port: PORT }, () => console.log(`Server reqdy at http://localhost:${PORT}${server.graphqlPath}`));
