4124caddcc89e79a53a730c167d523458bdd450f
dcDalin <mcdalinoluoch@gmail.com>
2019-08-19 12:57:56 +0300

    Setup linting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-const something = "hello";
+import express from 'express';
 
-console.log(something);
+const app = express();
+
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
